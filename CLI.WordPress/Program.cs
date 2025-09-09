4013b279a761335922b1288e143c404dc02a2fc6
Daniel Gonzalez <danielgonzalez10663@gmail.com>
2025-09-09 12:50:54 -0400

    Update Program.cs
    Added CRUD functionality

--- CLI.WordPress/Program.cs
+++ CLI.WordPress/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq.Expressions;
 using Library.WordPress.Models;
 
 namespace CLI.WordPress
@@ -8,8 +9,8 @@ namespace CLI.WordPress
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to WordPress!");
-            List<string?> blogPosts = new List<string?>();
-
+            List<Blog?> blogPosts = new List<Blog?>();
+            bool cont = true;
             do
             {
                 Console.WriteLine("C. Create a Blog Post");
@@ -19,37 +20,84 @@ namespace CLI.WordPress
                 Console.WriteLine("Q. Quit");
 
                 var userChoice = Console.ReadLine();
-
                 switch (userChoice)
                 {
                     case "C":
                     case "c":
-                        blogPosts.Add(Console.ReadLine() ?? string.Empty);
+                    {
+                        var blog = new Blog();
+                        blog.Title = Console.ReadLine();
+                        blog.Content = Console.ReadLine();
+                        var maxId = -1;
+                        if (blogPosts.Any())
+                        {
+                            maxId = blogPosts.Select(b => b?.Id ?? -1).Max();
+                        }
+                        else
+                        {
+                            maxId = 0;
+                        }
+                        blog.Id = ++maxId;
+                        blogPosts.Add(blog);
                         break;
+                    }
                     case "R":
                     case "r":
                         foreach(var b in blogPosts)
                         {
-                            Console.WriteLine($"({b?.Length}) {b}");
+                            Console.WriteLine(b);
                         }
                         break;
                     case "U":
                     case "u":
-                        break;
+                        {
+                            blogPosts.ForEach(Console.WriteLine);
+                            Console.WriteLine("Blog to Update (Id): ");
+                            var selection = Console.ReadLine();
+                            if (int.TryParse(selection, out int intSelection))
+                            {
+                                //get blog object
+                                var blogToUpdate = blogPosts
+                                    .Where(b => b != null)
+                                    .FirstOrDefault(b => (b?.Id ?? -1) == intSelection);
+                                //update it
+                                if (blogToUpdate != null)
+                                {
+                                    blogToUpdate.Title = Console.ReadLine();
+                                    blogToUpdate.Content = Console.ReadLine();
+                                }
+                            }
+                            break;
+                        }
                     case "D":
                     case "d":
+                    { 
+                        //display options to delete
+                        blogPosts.ForEach(Console.WriteLine);
+                        Console.WriteLine("Blog to Delete (Id): ");
+                        //get the blog user wants to 
+                        var selection = Console.ReadLine();
+                        if (int.TryParse(selection, out int intSelection))
+                        {
+                            //get blog object
+                            var blogToDelete = blogPosts
+                                .Where(b => b != null)
+                                .FirstOrDefault(b => (b?.Id ?? -1) == intSelection);
+                            //delete it!
+                            blogPosts.Remove(blogToDelete);
+                        }
                         break;
+                    }
                     case "Q":
                     case "q":
+                        cont = false;
                         break;
                     default:
                         Console.WriteLine("Invalid command");
                         break;
 
                 }
-
-
-            } while (true);
+            } while (cont);
         }
     }
 }
