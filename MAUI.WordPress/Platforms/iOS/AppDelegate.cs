461c1b5b47d1f1da442f8e2c8ca813dbebce616d
Daniel Gonzalez <danielgonzalez10663@gmail.com>
2025-09-15 10:02:16 -0400

    MAUI Initiation

---
+++ MAUI.WordPress/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MAUI.WordPress
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
